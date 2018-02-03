b6f269ef3749e3c41a3df182fe5e89a63771765b
Chao Shi <chris19891128@gmail.com>
2018-02-02 22:56:03 -0800

    110: check if balanced binary tree (Easy)

---
+++ 110_is_balanced_tree_e/main.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        def recur(root):
+            if root == None:
+                return 0
+            left = recur(root.left)
+            right = recur(root.right)
+            if left == -1 or right == -1 or abs(left - right) > 1:
+                return -1
+            return max(left, right) + 1
+        return recur(root) != -1
+
+# recur return one variable instead of (isBalanced, height)
