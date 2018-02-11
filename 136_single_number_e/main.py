34cdd0e155ee534334fde0f9666fb4192074fc12
Chao Shi <chris19891128@gmail.com>
2018-02-11 00:03:20 -0800

    136: single number (Easy)

---
+++ 136_single_number_e/main.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        xor = 0
+        for num in nums:
+            xor = num ^ xor
+        return xor
+            
