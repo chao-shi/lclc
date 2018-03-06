8c64595f1341b5c6baf24ccb0615b4e7fb510894
Chao Shi <chris19891128@gmail.com>
2018-03-05 21:57:28 -0800

    142: link list 2 find meet point (Medium)

---
+++ 142_link_list_2_m/main.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        sp, fp = head, head
+        meet = None
+        while fp:
+            fp = fp.next
+            if fp:
+                fp = fp.next
+                sp = sp.next
+            
+            if sp == fp:
+                meet = sp
+                break
+        
+        if not meet:
+            return None
+        
+        sp, fp = head, meet
+        while sp != fp:
+            sp = sp.next
+            fp = fp.next
+        return sp
+                
