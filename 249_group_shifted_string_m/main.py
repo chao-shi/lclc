3b17fb8d86ab06758bc2f092707c6a2057c909aa
Chao Shi <chris19891128@gmail.com>
2018-04-07 21:03:37 -0700

    249: Group shifted strings (Medium)

---
+++ 249_group_shifted_string_m/main.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def groupStrings(self, strings):
+        """
+        :type strings: List[str]
+        :rtype: List[List[str]]
+        """
+        def map_base(str):
+            res = ["a"]
+            for i in range(1, len(str)):
+                chr_ord = ord('a') + ord(str[i]) - ord(str[0])
+                if chr_ord > ord('z'):
+                    chr_ord -= 26
+                elif chr_ord < ord('a'):
+                    chr_ord += 26
+                res.append(chr(chr_ord))
+            return "".join(res)
+        
+        map = {}
+        for s in strings:
+            base = map_base(s)
+            map.setdefault(base, []).append(s)
+        
+        return [map[base] for base in map]
+
+# How to handle case of ["ba", "az"]
+# Add the difference as usual, if out of range, below a or above z, adjust by +/- 26
