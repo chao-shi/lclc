8763ba5c2b8a1c052053381d2b59a26c4bec3c8b
Chao Shi <chris19891128@gmail.com>
2018-04-12 22:01:32 -0700

    015_amend: Improve 3 sum question correctness

--- 015_3_sum_m/main.py
+++ 015_3_sum_m/main.py
@@ -12,19 +12,20 @@ class Solution(object):
             while j < k:
                 if nums[i] + nums[j] + nums[k] == 0:
                     ret.append([nums[i], nums[j], nums[k]])
-                    while j < k and nums[j] == nums[j+1]:
+                    j, k = j + 1, k - 1
+                    while j < k and nums[j] == nums[j-1]:
                         j += 1
-                    j += 1
-                    while j < k and nums[k] == nums[k-1]:
+                    while j < k and nums[k] == nums[k+1]:
                         k -= 1
-                    k -= 1
                 elif nums[i] + nums[j] + nums[k] < 0:
                     j += 1
                 else:
                     k -= 1
-            while i < len(nums) - 2 and nums[i] == nums[i+1]:
-                i += 1
             i += 1
+            while i < len(nums) - 2 and nums[i] == nums[i-1]:
+                i += 1
         return ret
 
 # Better, only do equal skip for case line 13
+# It is more correct on line 15 and 25 to change the index first and then do the while loop
+# e.g. line 25 i += 1, so i is moved to the next possible candidate first, which is checked upon i < len() - 2 next line
