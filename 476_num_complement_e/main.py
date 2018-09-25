777f88b040c9a245d069f23cab7cb82c58aabbc2
Chao Shi <chao.shi@hpe.com>
2018-09-25 00:05:37 -0700

    476: Number complement (Easy)

---
+++ 476_num_complement_e/main.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findComplement(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        mask = 1
+        while mask <= num:
+            mask <<= 1
+        mask -= 1
+        return (~num) & mask
