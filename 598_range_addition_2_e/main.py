c844880531c120284497931d82a993f038cc02a2
Chao Shi <chris19891128@gmail.com>
2018-10-28 00:20:41 -0700

    598: Range addition 2 (Easy)

---
+++ 598_range_addition_2_e/main.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def maxCount(self, m, n, ops):
+        """
+        :type m: int
+        :type n: int
+        :type ops: List[List[int]]
+        :rtype: int
+        """
+        if not ops:
+            return m * n
+        return min(map(lambda x:x[0], ops)) * min(map(lambda x:x[1], ops))
