2221b800cc6e1f8e8a41bd18db58a59340365ab8
Chao Shi <chris19891128@gmail.com>
2019-02-10 16:40:57 -0800

    119: pascal reverse

---
+++ 119_pascal_triangle_2_e/rev.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        res = [0] * (rowIndex + 1)
+        res[0] = 1
+        for i in range(rowIndex):
+            res[i + 1] = 1
+            for j in range(i, 0, -1):
+                res[j] = res[j] + res[j-1]
+        return res
