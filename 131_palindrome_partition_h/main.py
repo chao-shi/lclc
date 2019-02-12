2e04502f6e41eca37f31b6fc9cbcf4a18902fbf9
Chao Shi <chris19891128@gmail.com>
2019-02-11 22:33:56 -0800

    131: cleanup

--- 131_palindrome_partition_h/main.py
+++ 131_palindrome_partition_h/main.py
@@ -18,23 +18,12 @@ class Solution(object):
                     m[i][j] = (s[i] == s[j-1] and m[i+1][j-1])
             return m
         
-        n = len(s)
-        m = palinMap(s)
-        hm = {}
-        def recur(i):
-            if i in hm:
-                return hm[i]
-            elif i == n:
-                return [[]]
+        palin_map = palinMap(s)
+        palins = [[[]]]
+        for i, ch in enumerate(s):
             res = []
-            for j in range(i+1, n+1):
-                if m[i][j]:
-                    res.extend([s[i:j]] + ext for ext in recur(j))
-            hm[i] = res
-            return res
-        
-        if n == 0:
-            return []
-        return recur(0)
-        
-# Usually combination problem need block 36. Empty case need to return set of "phai" to make the derivation moves on
+            for j in range(i + 1):
+                if palin_map[j][i+1]:
+                    res.extend(prefix + [s[j:i+1]] for prefix in palins[j])
+            palins.append(res)
+        return palins[-1]
