24bc57a5f4c6efe45ea4009d8a66daa1e3404aee
Chao Shi <chris19891128@gmail.com>
2019-02-14 22:04:25 -0800

    209: cleanup sliding window

--- 209_min_size_subarr_sum_m/main.py
+++ 209_min_size_subarr_sum_m/main.py
@@ -5,20 +5,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        win_sum = 0
         j = 0
-        accj = 0
-        acci = 0
-        minwin = len(nums)
-        for i, num in enumerate(nums):
-            acci += nums[i]
-            while acci - accj >= s:
-                accj += nums[j]
-                j += 1
-            # sum(nums[j-1:i+1]) >= s
-            if j > 0:
-                minwin = min(minwin, i - j + 2)
-        return minwin if j > 0 else 0
-
-# Trick to assign minwin to be len(nums)
-# line 18
+        min_win = len(nums) + 1
+        for i in range(len(nums)):
+            win_sum += nums[i]
+            if win_sum >= s:
+                # all positive very important
+                # This allows us to greedily stop moving j
+                # once win_sum is below 2
+                while win_sum >= s:
+                    win_sum -= nums[j]
+                    j += 1
+                min_win = min(min_win, i - j + 2)
         
+        return min_win if min_win <= len(nums) else 0
+
+# Line 13 is important, two pointer sliding window always follow this pattern.
+# If current window ending at i satisfy, try moving j
+# If current window ending at i not satisfy, don't move j
