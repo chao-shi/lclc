5f2dce3a44cade5b8928acd0b05f1150a39c9a67
Chao Shi <chao.shi@hpe.com>
2019-02-24 17:34:39 -0800

    236: main1

---
+++ 236_lca_bt_m/main1.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if root == None:
+            return None
+
+        if root == p or root == q:
+            return root
+
+        left_lca = self.lowestCommonAncestor(root.left, p, q)
+        right_lca = self.lowestCommonAncestor(root.right, p, q)
+        
+        if right_lca:
+            return right_lca
+        elif left_lca:
+            return left_lca
+        else:
+            return root
+        
+# Single return vs 3 return approach
+# Single return: return lca, else return either p or q
+# Easier to write
+# Always need to traverse entire tree
