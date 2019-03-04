de4b19534e61ff05f69747cf54ee0804615634e4
Chao Shi <chris19891128@gmail.com>
2019-03-04 01:37:31 -0800

    392: cleanup

--- 392_is_subsequence_m/main.py
+++ 392_is_subsequence_m/main.py
@@ -5,10 +5,6 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if len(t) == 0:
-            # t[j] referenced in line 14
-            return len(s) == 0
-
         j = 0
         for i in range(len(s)):
             while j < len(t) and t[j] != s[i]:
