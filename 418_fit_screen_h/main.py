66624b0b6e97d8814c7672fdc41d4161107056ef
Chao Shi <chris19891128@gmail.com>
2019-03-06 23:42:33 -0800

    418: some comments

--- 418_fit_screen_h/main.py
+++ 418_fit_screen_h/main.py
@@ -23,3 +23,9 @@ class Solution(object):
 # i is a global index of infinite string 
 # Sentence A_B_C, then the infinite string is
 # A_B_C_A_B_C.....
+
+# Note if the line fills the word entirely, i is landing at the space after that
+# ["apple", "abble"]
+
+# line = 5, i stops at " "
+# line = 6, i stops at "a" from "abble"
