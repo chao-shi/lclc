53f091f5670e95c56f08dd279ffdf524a761ca8f
Chao Shi <chris19891128@gmail.com>
2019-03-06 23:42:46 -0800

    444: topo sort approach

---
+++ 444_reconstruct_seq_h/topo_sort.py
@@ -0,0 +1,53 @@
+class Solution(object):
+    def sequenceReconstruction(self, org, seqs):
+        """
+        :type org: List[int]
+        :type seqs: List[List[int]]
+        :rtype: bool
+        """
+        in_map, out_map = collections.defaultdict(set), collections.defaultdict(set)
+        elements = set()
+        for seq in seqs:
+            for e in seq:
+                elements.add(e)
+            for i in range(1, len(seq)):
+                in_map[seq[i]].add(seq[i-1])
+            for i in range(len(seq) - 1):
+                out_map[seq[i]].add(seq[i+1])
+                
+        # find one with no in_degree
+        src = filter(lambda x:len(in_map[x]) ==0, elements)
+        
+        if len(src) != 1:
+            return False
+        
+        src = src[0]
+        visited = {}
+        rev_seq = []
+        
+        def topo_sort(cur):
+            if visited.get(cur, 0) == 1:
+                return False
+            elif visited.get(cur, 0) == 2:
+                return True
+
+            visited[cur] = 1
+            for next in out_map.get(cur, []):
+                if not topo_sort(next):
+                    return False
+            visited[cur] = 2
+            rev_seq.append(cur)
+            return True
+        
+        topo_sort(src)
+        
+        # case where the component of src is good, but another component is a loop.
+        if len(visited) != len(elements):
+            return False
+        
+        return rev_seq[::-1] == org
+
+# This is clearer than the PQ approach
+# It picks one node with zero in-degree and does the topo-sort from there
+# Check if all nodes are visited and the topology sort return same as the origin 
+                
