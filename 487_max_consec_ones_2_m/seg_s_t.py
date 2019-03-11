6347c40b5ad08ee7e8024fb6d98bcd8232567861
Chao Shi <chris19891128@gmail.com>
2019-03-10 22:44:52 -0700

    487:clean up seg window algorithm

--- 487_max_consec_ones_2_m/seg_s_t.py
+++ 487_max_consec_ones_2_m/seg_s_t.py
@@ -4,29 +4,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # Keep (s, t) of current 1 seg and last one
-        max_win = 0
-        last_s, last_t = None, None
-        cur_s, cur_t = None, None
-        for i, num in enumerate(nums):
-            if num == 1:
-                if cur_s == None:
-                    cur_s = i
-                cur_t = i + 1
-                # flip 0 before 1 seg
-                max_win = max(max_win, cur_t - cur_s + (1 if cur_s > 0 else 0))
+        anchor = -1
+        maxv = 0
+        last_win = None
+        for i in range(len(nums) + 1):
+
+            if i == len(nums) or nums[i] == 0:
+                seg_len = i - anchor - 1
                 
-                # flip 0 in between
-                if last_t != None and last_t + 1 == cur_s:
-                    max_win = max(max_win, cur_t - last_s)
-            else:
-                if cur_s != None:
-                    # flip 0 after 1 seg
-                    max_win = max(max_win, cur_t - cur_s + 1)
-                last_s, last_t = cur_s, cur_t
-                cur_s, cur_t = None, None
-                max_win = max(max_win, 1)
-        return max_win
+                maxv = max(maxv, seg_len)
+                if anchor >= 0 or i < len(nums):
+                    maxv = max(maxv, seg_len + 1)    
                 
-# Don't forget case 23
-# Works for stream
+                if last_win != None:
+                    s, t = last_win
+                    if t == anchor:
+                        maxv = max(maxv, seg_len + 1 + t - s)
+
+                last_win = (anchor + 1, i)
+                anchor = i
+        return maxv
