2778c8f1f8d34a0bb2f391f71646a38b3d421bc6
Chao Shi <chao.shi@hpe.com>
2019-03-13 21:32:55 -0700

    523: correct for at least two elements sub array

--- 523_continuous_subarr_sum_m/main.py
+++ 523_continuous_subarr_sum_m/main.py
@@ -11,13 +11,15 @@ class Solution(object):
         if k == 0:
             return any(nums[i] == nums[i-1] == 0 for i in range(1, len(nums)))
 
-        accu_mod_map = set([0, nums[0] % k])
+        accu_mod_map = set([0])
+        last_accu_sum = 0
         accu_sum = nums[0]
         for i in range(1, len(nums)):
+            last_accu_sum = accu_sum
             accu_sum = nums[i] + accu_sum
             if accu_sum % k in accu_mod_map:
                 return True
-            accu_mod_map.add(accu_sum % k)
+            accu_mod_map.add(last_accu_sum % k)
         return False
     
 # Careful case when k = 0
