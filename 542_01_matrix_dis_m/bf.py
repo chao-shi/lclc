f69b393827ea7f470c6ccd8cba0196f89f2cb62c
Chao Shi <chao.shi@hpe.com>
2019-03-14 22:21:40 -0700

    542: multi source BFS

--- 542_01_matrix_dis_m/bf.py
+++ 542_01_matrix_dis_m/bf.py
@@ -5,40 +5,27 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         m, n = len(matrix), len(matrix[0])
-
-        def inside(x, y):
-            return 0 <= x < m and 0 <= y < n
-
-        def bfs(x, y):
-            q = collections.deque()
-            q.append((x, y))
-            depth = 1
-            visited = set([(x, y)])
-
-            while q:
-                cur_len = len(q)
-                for _ in range(cur_len):
-                    x1, y1 = q.popleft()
-                    for v in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-                        x2, y2 = x1 + v[0], y1 + v[1]
-                        if not inside(x2, y2) or (x2, y2) in visited:
-                            continue
-                        elif matrix[x2][y2] == 0:
-                            return depth
-                        else:
-                            visited.add((x2, y2))
-                            q.append((x2, y2))
-                depth += 1
-                
-                
         res = [[0] * n for i in range(m)]
         for i in range(m):
             for j in range(n):
                 if matrix[i][j] == 1:
-                    res[i][j] = bfs(i, j)
+                    res[i][j] = m * n + 1
+
+        zeros = [(i, j) for i in range(m) for j in range(n) if matrix[i][j] == 0]
+
+        q = collections.deque()
+        q.extend(zeros)
+        
+        while q:
+            x, y = q.popleft()
+            for v in [(0, 1), (1, 0), (0, -1),(-1, 0)]:
+                xx, yy = x + v[0], y + v[1]
+                if 0 <= xx < m and 0 <= yy < n and res[xx][yy] > res[x][y] + 1:
+                    res[xx][yy] = res[x][y] + 1
+                    q.append((xx, yy))
         return res
-                            
-                
-# Passes OJ. I though about solving like maze shortest distance to exit first. 
-# Maze problem has very few exits
-# But not feasible with too many zeroes here
+
+# Multiple start BFS.
+# To avoid using visited variable few choices
+# 1. Traverse layer by layer, each layer use set
+# 2. Use the distance map, and only push to queue when strictly smaller found
