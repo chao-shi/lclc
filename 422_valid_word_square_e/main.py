da6d94565a4f1985d3fa989356cc72dae3a9768b
Chao Shi <chao.shi@hpe.com>
2019-05-09 18:05:03 -0700

    422: cleanup:

--- 422_valid_word_square_e/main.py
+++ 422_valid_word_square_e/main.py
@@ -4,19 +4,10 @@ class Solution(object):
         :type words: List[str]
         :rtype: bool
         """
+        if len(words) != len(words[0]):
+             return False
         for i, w in enumerate(words):
-            column = ""
-            for j in range(len(words)):
-                if i < len(words[j]):
-                    column += words[j][i]
-                else:
-                    column += " "
-            
-            if column[:len(w)] != w:
+            col = "".join([ww[i] for ww in words if i < len(ww)])
+            if w != col:
                 return False
-
-            # checking trailing spaces, here for sure len(column) >= len(w)
-            if any(ch != " " for ch in column[len(w):]):
-                return False
-            
         return True
