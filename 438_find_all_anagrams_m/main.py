e6fe510903994267c95fcf101fd6f2e4d9736466
Chao Shi <chao.shi@hpe.com>
2019-05-10 23:50:03 -0700

    438: cleanup

--- 438_find_all_anagrams_m/main.py
+++ 438_find_all_anagrams_m/main.py
@@ -5,38 +5,25 @@ class Solution(object):
         :type p: str
         :rtype: List[int]
         """
-        if len(s) < len(p):
-            return []
-        
-        win = {}
-        for i in range(len(p)):
-            win[p[i]] = win.get(p[i], 0) - 1
-            win[s[i]] = win.get(s[i], 0) + 1
-
-        win = {ch:win[ch] for ch in win if win[ch] != 0}
+        win = collections.defaultdict(int)
+        counter = collections.Counter(p)
+        i = 0
         res = []
-        
-        # Don't forget corner case. First pair or last pair
-        # Here we update win first then check, so last pair is considered in the loop
-        # But not first pair
-        if len(win) == 0:
-            res.append(0)
-
-        for i in range(len(p), len(s)):
-            j = i - len(p)
+        matched = 0
+        for j in range(len(s)):
+            win[s[j]] += 1
+            if win[s[j]] <= counter.get(s[j]):
+                matched += 1
                 
-            win[s[i]] = win.get(s[i], 0) + 1
-            if win[s[i]] == 0:
-                del win[s[i]]
-
-            win[s[j]] = win.get(s[j], 0) - 1
-            if win[s[j]] == 0:
-                del win[s[j]]
+            while j - i + 1 > len(p):
+                win[s[i]] -= 1
+                if win[s[i]] < counter.get(s[i]):
+                    matched -= 1
+                i += 1
+            
+            if matched == len(p) and j - i + 1 == len(p):
+                res.append(i)
                 
-            if len(win) == 0:
-                res.append(j + 1)
-        
         return res
-    
-# Careful about corner case
-# 
+        
+        
