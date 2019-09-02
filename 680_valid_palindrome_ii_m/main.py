2722c0deafcd094ce64140a9a837b4027d29ed6f
Chao Shi <chao.shi@hpe.com>
2019-09-02 19:13:59 -0400

    680_palindrome ii (Medium)

--- 680_valid_palindrome_ii_m/main.py
+++ 680_valid_palindrome_ii_m/main.py
@@ -4,13 +4,10 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        s_mirror = s[::-1]
-        i, j = 0, 0
-        n = len(s)
-        while i < n:
-            if s[i] == s_mirror[i]:
-                i += 1
+        i, j = 0, len(s) - 1
+        while i < j:
+            if s[i] == s[j]:
+                i, j = i + 1, j - 1
             else:
-                # Try delete s[i] or s_mirror[i], which is also s[n - i - 1]
-                return s[i+1:] == s_mirror[i:n-i-1] + s_mirror[n-i:] or s[i:n-i-1] + s[n-i:] == s_mirror[i+1:]
+                return s[i:j] == s[i:j][::-1] or s[i+1:j+1] == s[i+1:j+1][::-1]
         return True
